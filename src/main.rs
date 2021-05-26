48ba9c70ab0d039549ae2e585c767458cda30679
jzx <945627077@qq.com>
2021-05-26 17:47:54 +0800

    测试trait实现

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ fn main() {
     let mut a=20;
     let  b=30;
     my::func1(&mut a);
-    println!("{},{}",a,b);
+    my::func2::<&mut i32>(&mut a);
 
+    my::func3();
+    println!("{},{}",a,b);
 }
 
 
--- src/my/mod.rs
+++ src/my/mod.rs
@@ -2,3 +2,54 @@ pub fn func1( a: &mut i32){
     *a=400;
     println!("{}",&a);
 }
+
+
+pub fn func2<T: std::fmt::Display>( a: T){
+    println!("泛型函数xxxxxxxxxxxxxx");
+    println!("{}",&a);
+}
+
+pub fn func3(){
+   let u1=U1{
+       name:String::from("狗的不行的rust"),
+   };
+   u1.hello();
+
+   let u2=U2{
+        name:String::from("狗的不行的rust2"),
+    };
+    u2.hello();
+}
+
+
+
+pub struct U1 {
+    pub name:String,
+
+} 
+
+impl Say for U1 {
+    fn name(&self) -> String {
+        return self.name.clone();
+    }
+
+}
+
+pub struct U2 {
+    pub name:String,
+} 
+
+impl Say for U2 {
+    fn name(&self) -> String {
+        return self.name.clone();
+    }
+}
+
+
+
+pub trait Say {
+    fn name(&self) -> String;
+    fn hello(&self)  {
+        println!("说话 {}",self.name())
+    }
+}
