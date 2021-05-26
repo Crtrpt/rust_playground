d883abd14c20cb73e20cb0c4ccc40a9e71cbf06f
jzx <945627077@qq.com>
2021-05-26 17:08:19 +0800

    addMod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod my;
+
 fn main() {
-    println!("Hello, world!");
+    let mut a=20;
+    let  b=30;
+    my::func1(&mut a);
+    println!("{},{}",a,b);
+
 }
+
+
---
+++ src/my/mod.rs
@@ -0,0 +1,4 @@
+pub fn func1( a: &mut i32){
+    *a=400;
+    println!("{}",&a);
+}
